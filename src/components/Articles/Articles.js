69388c640fb2771a7b6951ff014d1ccf6daa5693
Dawid Loranc <dawidloranc@gmail.com>
2019-11-11 19:02:31 +0100

    Add another version of request cancellation without global variable

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,18 +1,17 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { CancelToken } from 'axios';
 import { fetchArticles } from './../../store/articles/actionCreators';
 import { setShow } from './../../store/show/actionCreators';
 
-window.source = CancelToken.source();
-
 export const Articles = ({ show, articles, setShow, fetchArticles }) => {
   useEffect(() => {
-    fetchArticles();
+    const { request, abort } = fetchArticles();
+
+    request();
 
     setTimeout(() => {
       setShow(false);
-      window.source.cancel('Request aborted');
+      abort();
     }, 1)
   }, [fetchArticles, setShow]);
 
--- src/store/articles/actionCreators.js
+++ src/store/articles/actionCreators.js
@@ -1,29 +1,36 @@
 import { createAction } from 'redux-starter-kit';
-import axios from 'axios';
+import axios, { CancelToken } from 'axios';
 import { normalize, schema } from 'normalizr';
 
 export const addArticle = createAction('articles/ADD');
 export const setArticles = createAction('articles/SET');
 
-export const fetchArticles = () => {
-  return async dispatch => {
-    axios.get('http://localhost:4000/articles', {
-      cancelToken: window.source.token,
-    }).then(response => {
-      const { data } = response;
+export const fetchArticles = () => dispatch => {
+  const source = CancelToken.source();
 
-      const tag = new schema.Entity("tags", {});
-      const article = new schema.Entity("articles", {
-        tags: [tag]
-      });
+  return {
+    request: async () => {
+      return axios.get('http://localhost:4000/articles', {
+        cancelToken: source.token,
+      }).then(response => {
+        const { data } = response;
 
-      const normalizedData = normalize(data, [article]);
+        const tag = new schema.Entity("tags", {});
+        const article = new schema.Entity("articles", {
+          tags: [tag]
+        });
 
-      const articles = normalizedData.result.map(
-        result => normalizedData.entities.articles[result]
-      );
+        const normalizedData = normalize(data, [article]);
 
-      dispatch(setArticles(articles));
-    }).catch(error => console.log(error.message));
+        const articles = normalizedData.result.map(
+          result => normalizedData.entities.articles[result]
+        );
+
+        dispatch(setArticles(articles));
+      }).catch(error => console.log(error.message))
+    },
+    abort: () => {
+      source.cancel('Request aborted');
+    }
   }
 }
